aec6d19acf1e769fd975c5855f79e26f0745b865
NielsVancraeynest <105343495+NielsVancraeynest@users.noreply.github.com>
2022-05-11 16:17:05 +0200

    Update Analytical_clinch.py

--- Analytical_clinch.py
+++ Analytical_clinch.py
@@ -154,7 +154,7 @@ def show_page():
     else:
         try:
             # read the uploaded file
-            df = pd.read_excel(io=uploadedFile.name) # it is important that the titles in the excel are indentical to those used in def 'getForce'
+            df = pd.read_excel(uploadedFile) # it is important that the titles in the excel are indentical to those used in def 'getForce'
             # display the database
             st.dataframe(df)
             # import al the data into de dictionary 'val'
