bf1dbb32e99eeba143fe64139ffc021bbdb7dc38
KULeuven-ElooiLab <105343495+KULeuven-ElooiLab@users.noreply.github.com>
2022-05-23 20:30:39 +0200

    Update ML_clinch.py

--- ML_clinch.py
+++ ML_clinch.py
@@ -157,8 +157,8 @@ def show_page():
     image2 = Image.open('docs/InputML2.jpg')
     #-- Make it posseble to overlay the image with text 
     colour = (50,50,50)
-    font = ImageFont.truetype( 17)
-    font1 = ImageFont.truetype(15)
+    font = ImageFont.truetype('/System/Library/Fonts/Arial', 17)
+    font1 = ImageFont.truetype('/System/Library/Fonts/Arial',15)
 
     #-- Write each parameter on the image based on the pixel coordinate
     # -----Tools-----
