4a0224515031fbe3af844495d27151e3b8bff45c
kuleuven-elooilab <105343495+kuleuven-elooilab@users.noreply.github.com>
2022-07-01 15:03:12 +0200

    Update WorkflowVoorML.py

--- WorkflowVoorML.py
+++ WorkflowVoorML.py
@@ -512,9 +512,8 @@ def feature_ML_Train():
                     #     except:
                     #         st.error('There is no trained model available')
                     
-        st.write(st.session_state['Algorithm'])
         if st.session_state["best_model"]!=0:
-                        st.download_button('Download the trained model',pickle.dumps([st.session_state['best_model'],list(X.keys()),train_with]), train_with+st.session_state['Algorithm']+".pkl")
+                        st.download_button('Download the trained model',pickle.dumps([st.session_state['best_model'],list(X.keys()),train_with]), train_with+"_"+st.session_state['Algorithm']+".pkl")
                     
 def feature_ML_Predict():
     text.WF_function_ML_Predict()
